8a6284755f913fccd01e0b0553ed298ae933db4a
beeno24 <77907170+beeno24@users.noreply.github.com>
2022-02-05 23:01:47 -0600

    box go fidget spinner

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let xPos = 200;
 let yPos = 200;
 
+let angle = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
     
   rectMode(CENTER);
   //centers at coord.
@@ -12,8 +14,15 @@ function draw() {
   background(220);
   //changes bg to monocrom color (can be hex)
   
+  if (keyIsDown(16)) {
+    rotate(angle);
+  }
+//rotates the box when shift is clicked
+  
+  //rotates box
   rect(xPos,yPos,200,200);
   //creates rectangle @ 50,50 coord.
+  angle += 0.07;
   
   if (keyIsDown(UP_ARROW)) {
     yPos -= 10;
@@ -32,5 +41,5 @@ function draw() {
   }
   
   
+  
 }
-
