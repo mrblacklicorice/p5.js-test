efa31a8c62db59f78023c4561ee4032eedca2b52
beeno24 <77907170+beeno24@users.noreply.github.com>
2022-02-05 22:46:17 -0600

    coordinate setting

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
   createCanvas(400, 400);
+  
+  rectMode(CENTER);
+  //centers at coord.
 }
 
 function draw() {
   background(220);
+  //changes bg to monocrom color (can be hex)
+  
+  rect(200,200,200,200);
+  //creates rectangle @ 50,50 coord.
+  
+  
 }
